19e371d1eea9f63e629512f877a1a22f7eae191f
tinega gideon <118301331+tin3ga@users.noreply.github.com>
2022-12-08 21:58:48 +0300

    Fix: 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
-#include <string.h>
 
-int main () 
+/**
+  *put() function write string to the stdout
+  */
+
+int main(void) 
 {
 	char my_string[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(my_string);
 
-	return(0);
+	return (0);
 
 }
 
