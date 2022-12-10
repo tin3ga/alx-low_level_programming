b8fdbe2bb282fdc4d100dc377d2b608736ff1809
tinega gideon <118301331+tin3ga@users.noreply.github.com>
2022-12-10 11:55:18 +0300

    Add 8-print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main : main code block
+ * Description: print numbers of base 16
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	char k;
+
+	for (1 = 0 ; i < 10 ; i++)
+	{
+		putchar(i + '0');
+	}
+	for (k = 'a'; k <= 'f' ; k++)
+	{
+		putchar(k);
+	}
+	putchar('\n');
+	return (0);
+}
+
