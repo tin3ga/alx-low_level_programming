8ce2f6b1d5b08129ff9328d3ad058c719fa9ee63
tinega gideon <118301331+tin3ga@users.noreply.github.com>
2022-12-10 10:08:12 +0300

    Add 5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main :  main block block
+ * while loops
+ * Description: Print all numbers of base 10
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	while (i < 10)
+	{
+		printf("%d", i);
+		i++;
+	}
+	printf("\n");
+
+	return (0);
+}
