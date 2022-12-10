103e78fcd46aec32ec6867fd8cd1b0e70feaaafa
tinega gideon <118301331+tin3ga@users.noreply.github.com>
2022-12-10 09:22:56 +0300

    Fix: 2-print_alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,15 +5,12 @@
  * Return: 0
  */
 int main(void)
-{ 
+{
 	char alphabet;
-
-        for (alphabet = 'a';alphabet <= 'z' ;alphabet ++)
-        {
-       		putchar(alphabet);
-        }
-
-        putchar('\n');
-
-        return (0);
+	for(alphabet = 'a' ; alphabet <= 'z' ; alphabet ++ )
+	{
+		putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
 }
