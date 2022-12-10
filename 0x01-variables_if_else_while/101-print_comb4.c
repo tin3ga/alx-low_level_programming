d36e6e7ba87dbcbfe194254e24c79e3d55756a8e
tinega gideon <118301331+tin3ga@users.noreply.github.com>
2022-12-10 12:12:45 +0300

    Add 101-print_comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Print all possible different combinations of 3 digits.
+ * Numbers must be separated by commas and a space.
+ * 012, 120, 102, 021, 201, 210 are considered the same combination.
+ * Return: 0
+ */
+int main(void)
+{
+	int a, b, c, d;
+
+	for (a = 0; a < 1000; a++)
+	{
+		b = a / 100; 
+		c = (a / 10) % 10; 
+		d = a % 10; 
+
+		if (b < c && c < d)
+		{
+			putchar(b + '0');
+			putchar(c + '0');
+			putchar(d + '0');
+
+			if (a < 700)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
