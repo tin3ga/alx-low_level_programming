92dbb1fb403176d171ca321f46ec59687d3a825b
tinega gideon <118301331+tin3ga@users.noreply.github.com>
2022-12-14 12:21:49 +0300

    Add 4-isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * _isalpha : check for alphabetic character
+  *
+  *
+  * Return 1 : for lowercase or uppercase letter
+  * Return 0 : for other
+  */
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122 || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+
+	return (0);
