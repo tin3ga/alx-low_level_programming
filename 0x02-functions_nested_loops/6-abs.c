a405604cd99bfb446479a249c30229b0f5c4337c
tinega gideon <118301331+tin3ga@users.noreply.github.com>
2022-12-14 23:55:08 +0300

    Add 6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  * _abs : compute absolute value of an int
+  *
+  * Return : Int
+  */
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
