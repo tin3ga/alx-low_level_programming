83ef678677aa0b465921d261233fe11f0ce0a3bb
tinegagideon <118301331+tin3ga@users.noreply.github.com>
2022-12-22 14:50:56 +0300

    add 4-print_rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string in reverse.
+ * @s: string to be reserved.
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (*(s + i))
+		i++;
+	i = i - 1;
+	while (i >= 0)
+	{
+		_putchar(*(s + i));
+		i--;
+	}
+	_putchar('\n');
+}
